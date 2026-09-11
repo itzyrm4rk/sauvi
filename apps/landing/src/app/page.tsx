d187a9d42e4e06e95847a12367d248242d373e79
didieratgmarc@gmail.com <didieratgmarc@gmail.com>
2026-09-11 08:51:59 +0100

    API E2E test

--- apps/landing/src/app/page.tsx
+++ apps/landing/src/app/page.tsx
@@ -9,13 +9,9 @@ export default function Home() {
   return (
     <main style={{ position: 'relative', minHeight: '100vh', zIndex: 1 }}>
       <Header />
-      {/* Hero has id="telechargement" */}
       <Hero />
-      {/* FeaturesShowcase has id="showcase" */}
       <FeaturesShowcase />
-      {/* InstallGuide has id="installation" */}
       <InstallGuide />
-      {/* BloodChart has id="compatibilite" */}
       <BloodChart />
       <Footer />
     </main>
